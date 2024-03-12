e74079dce501ff33ac03de25a3d22742aeebffe1
Phil Freeman <phil@hasura.io>
2024-03-12 16:05:40 -0700

    ndc-test: single_column aggregates

--- ndc-test/src/test_cases/query/mod.rs
+++ ndc-test/src/test_cases/query/mod.rs
@@ -58,6 +58,7 @@ pub async fn test_query<C: Connector, R: Reporter>(
                                 reporter,
                                 schema,
                                 collection_info,
+                                rng,
                             )
                         });
                     }
