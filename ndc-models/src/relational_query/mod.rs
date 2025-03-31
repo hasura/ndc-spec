89fbffa68203d8128611c78836073bdff8459379
Daniel Chambers <daniel@hasura.io>
2025-03-31 17:13:42 +1100

    Rework scopes, add arc-relation feature

--- ndc-models/src/relational_query/mod.rs
+++ ndc-models/src/relational_query/mod.rs
@@ -28,46 +28,145 @@ pub struct RelationalQueryResponse(Vec<serde_json::Value>);
 #[serde(tag = "type")]
 #[schemars(title = "Relation")]
 pub enum Relation {
+    /// Translates to SQL approximately:
+    /// ```sql
+    /// SELECT <scope_name>.<column0> [, <scope_name>.<columnN> ...]
+    /// FROM <collection> AS <scope_name>
+    /// ```
+    /// Introduces a new scope named `scope_name` that `columns` will be available on.
     From {
         collection: CollectionName,
         columns: Vec<FieldName>,
         scope_name: ScopeName,
     },
+    /// Translates to SQL approximately:
+    /// ```sql
+    /// <input>
+    /// [LIMIT <fetch>] OFFSET <skip>
+    /// ```
+    /// Does not introduce a new scope.
     Paginate {
+        #[cfg(not(feature = "arc-relation"))]
         input: Box<Relation>,
+        #[cfg(feature = "arc-relation")]
+        input: std::sync::Arc<Relation>,
+
         fetch: Option<u64>,
         skip: u64,
     },
+    /// Translates to SQL approximately:
+    /// ```sql
+    /// SELECT *
+    /// FROM (
+    ///     SELECT <expr0> AS <expr0_field_name> [, <exprN> AS <exprN_field_name> ...]
+    ///     <input>
+    /// ) AS <scope_name>
+    ///
+    /// ```
+    /// `exprs` will refer to scopes defined inside input.
+    /// This introduces a new scope named `scope_name` that hides the scopes defined inside input.
+    /// The projected `exprs` will be available on this new scope for relations that wrap this one.
     Project {
+        #[cfg(not(feature = "arc-relation"))]
         input: Box<Relation>,
+        #[cfg(feature = "arc-relation")]
+        input: std::sync::Arc<Relation>,
+
         exprs: IndexMap<FieldName, RelationalExpression>,
         scope_name: ScopeName,
     },
+    /// Translates to SQL approximately:
+    /// ```sql
+    /// <input>
+    /// WHERE <predicate>
+    /// ```
+    /// This does not introduce a new scope, so `predicate` exprs will refer to the scopes defined inside input
     Filter {
+        #[cfg(not(feature = "arc-relation"))]
         input: Box<Relation>,
+        #[cfg(feature = "arc-relation")]
+        input: std::sync::Arc<Relation>,
+
         predicate: RelationalExpression,
     },
+    /// Translates to SQL approximately:
+    /// ```sql
+    /// <input>
+    /// ORDER BY <exprs0.expr> <exprs0.direction> <exprs0.nulls_sort: NULLS FIRST|LAST> [, <exprsN.expr> <exprsN.direction> <exprsN.nulls_sort: NULLS FIRST|LAST> ...]
+    /// ```
+    /// This does not introduce a new scope, so `sort` exprs will refer to the scopes defined inside input
     Sort {
+        #[cfg(not(feature = "arc-relation"))]
         input: Box<Relation>,
+        #[cfg(feature = "arc-relation")]
+        input: std::sync::Arc<Relation>,
+
         exprs: Vec<Sort>,
     },
+    /// Translates to SQL approximately:
+    /// ```sql
+    /// <left>
+    /// <join_type> JOIN <right> AS <right_scope_name>
+    /// ON <on0.left> = <on0.right> [, <onN.left> = <onN.right> ...>
+    /// ```
+    /// Introduces a new scope for the right relation (right_scope_name) without hiding the left relation's scope.
+    /// `on` exprs will refer to both the scope defined inside left and the right_scope_name.
     Join {
+        #[cfg(not(feature = "arc-relation"))]
         left: Box<Relation>,
-        left_scope_name: ScopeName,
+        #[cfg(feature = "arc-relation")]
+        left: std::sync::Arc<Relation>,
+
+        #[cfg(not(feature = "arc-relation"))]
         right: Box<Relation>,
+        #[cfg(feature = "arc-relation")]
+        right: std::sync::Arc<Relation>,
+
         right_scope_name: ScopeName,
+
         on: Vec<JoinOn>,
         join_type: JoinType,
     },
+    /// Translates to SQL approximately:
+    /// ```sql
+    /// SELECT *
+    /// FROM (
+    ///     SELECT <aggregates0.expr> AS <aggregates0.field_name> [, <aggregatesN.expr> AS <aggregatesN.field_name> ...]
+    ///     <input>
+    ///     [GROUP BY <group_by0> [, <group_byN> ...]]
+    /// ) AS <scope_name>
+    /// ```
+    /// `aggregates` and `group_by` expressions will refer to scopes defined inside input.
+    /// This introduces a new scope named `scope_name` that hides the scopes defined inside input.
+    /// The calculated `aggregates` will be available on this new scope for relations that wrap this one.
     Aggregate {
+        #[cfg(not(feature = "arc-relation"))]
         input: Box<Relation>,
+        #[cfg(feature = "arc-relation")]
+        input: std::sync::Arc<Relation>,
+
         /// Only non-empty if the 'relational_query.aggregate.group_by' capability is supported.
         group_by: Vec<RelationalExpression>,
         aggregates: IndexMap<FieldName, RelationalExpression>,
         scope_name: ScopeName,
     },
+    /// Translates to SQL approximately:
+    /// ```sql
+    /// SELECT *
+    /// FROM (
+    ///     SELECT <exprs0> AS <exprs0_field_name> [, <exprsN> AS <exprsN_field_name> ...]
+    ///     <input>
+    /// ) AS <scope_name>
+    /// ```
+    /// `exprs` will refer to scopes defined inside input.
+    /// This introduces a new scope named `scope_name` that hides the scopes defined inside input.
+    /// The projected `exprs` will be available on this new scope for relations that wrap this one.
     Window {
+        #[cfg(not(feature = "arc-relation"))]
         input: Box<Relation>,
+        #[cfg(feature = "arc-relation")]
+        input: std::sync::Arc<Relation>,
+
         exprs: IndexMap<FieldName, RelationalExpression>,
         scope_name: ScopeName,
     },
