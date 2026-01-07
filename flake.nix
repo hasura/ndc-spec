1e7554301a5ca8942d7568314b1cdac043af362f
Daniel Harvey <danieljamesharvey@gmail.com>
2026-01-07 14:01:19 +0000

    Bump nix flake too

--- flake.nix
+++ flake.nix
@@ -40,9 +40,6 @@
         ];
       runtimeDependencies =
         pkgs.lib.optionals pkgs.stdenv.isDarwin [
-          pkgs.darwin.apple_sdk.frameworks.CoreFoundation
-          pkgs.darwin.apple_sdk.frameworks.Security
-          pkgs.darwin.apple_sdk.frameworks.SystemConfiguration
           pkgs.libiconv
         ];
 
